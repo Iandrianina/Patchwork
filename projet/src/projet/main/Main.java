c343e8d0d84232e9fc7691ba2291fc204ba2d21f
gael-bit <40693842+gael-bit@users.noreply.github.com>
2022-11-28 13:55:54 +0100

    Ajout des deux première pièces

--- projet/src/projet/main/Main.java
+++ projet/src/projet/main/Main.java
@@ -2,6 +2,14 @@ package projet.main;
 
 public class Main {
 	public static void main(String args[]) {
-		
+	  int[][] tableau;
+	  tableau = new int[2][2];
+	  for(int i = 0; i < tableau.length; i++) {
+	    for(int j = 0; j < tableau.length; j++) {
+	      tableau[i][j] = 1;
+	    }
+	  }
+		var listOfPatch = List.of(
+		    new Patch(1,3,4,));
 	}
 }
--- projet/src/projet/patchwork/Patch.java
+++ projet/src/projet/patchwork/Patch.java
@@ -2,7 +2,7 @@ package projet.patchwork;
 
 import java.util.Objects;
 
-public record Patch(int priceButton, int nbSpaces, int[][] shape) {
+public record Patch(int nbButton, int priceButton, int nbSpaces, int[][] shape) {
 	public Patch {
 		Objects.requireNonNull(priceButton);
 		Objects.requireNonNull(nbSpaces);
